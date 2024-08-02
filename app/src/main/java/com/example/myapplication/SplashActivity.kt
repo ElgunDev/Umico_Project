2c5c97d5ccf87e3d43a53d0882a87d0419fa4149
ElgunDev <elgunhuseynzade20@gmail.com>
2024-08-02 19:23:24 +0400

    First_Branch

--- app/src/main/java/com/example/myapplication/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -1,17 +1,23 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.matrix.android105_android.R
 
-class MainActivity: AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_splash)
+        val splashScreenTime =3000L
+        Handler().postDelayed({
+            val intent = Intent(this , MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } , splashScreenTime)
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    kotlin("kapt") version "2.0.0"
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id ("androidx.navigation.safeargs.kotlin") version  "2.5.1" apply false
 }
