72edc0da91301496dc24ff697d1b71c52cbf4ef6
ElgunDev <elgunhuseynzade20@gmail.com>
2024-08-02 19:23:24 +0400

    First_Branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,22 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.devtools.ksp")
+    id("kotlin-kapt")
+    id ("androidx.navigation.safeargs.kotlin")
+
+
+
+
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.matrix.android105_android"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.matrix.android105_android"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -26,6 +34,8 @@ android {
             )
         }
     }
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -33,16 +43,45 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    val room_version = "2.6.1"
+    val retrofit_version = "2.11.0"
+    val lifecycle_version = "2.8.1"
+    val fragment_version = "1.7.1"
+
+    implementation ("androidx.room:room-runtime:$room_version")
+
+
+
+//    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    kapt("androidx.room:room-compiler:$room_version")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.core:core-splashscreen:1.0.0")
+}
+kapt{
+    correctErrorTypes = true
 }
